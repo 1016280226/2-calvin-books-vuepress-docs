57087b8a3364f70bf74f74280477f3d53d86182c
Calvin_1016280226 <1016280226@qq.com>
2020-07-12 02:04:41 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,105 +5,105 @@ module.exports = {
     },
     // 头部
     head: [
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ['link', { rel: 'icon', href: '/logo/small.png' }]
+        ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
+        ['link', {rel: 'icon', href: '/logo/small.png'}]
     ],
     // 网站标题及描述
-    title: 'EL-ADMIN',
-    description: '一个简单且易上手的 Spring boot 后台管理框架',
+    title: 'Calvin Books',
+    description: '自律成就自由，学习改变未来 ！',
     // 主题配置
     themeConfig: {
         // 提取markdown中h2 和 h3 标题，显示在侧边栏上。
         sidebarDepth: 2,
-        // 文档更新时间
         lastUpdated: '更新时间',
         // logo
         logo: '/logo/small.png',
         nav: [
-            { text: '指南', link: '/guide/'},
-            { text: '日志', link: '/version/V2.5/'},
-            { text: '捐赠', link: '/donation/'},
-            { text: '体验', link: 'https://el-admin.xin'},
-            { text: '博客', link: 'https://www.ydyno.com/'},
-            // 下拉列表
+            {text: '首页', link: '/books/'}
+            // { text: '', link: '/version/V2.5/'},
+            // { text: '捐赠', link: '/donation/'},
+            // { text: '体验', link: 'https://el-admin.xin'},
+            // { text: '博客', link: 'https://www.ydyno.com/'},
+            //下拉列表
+            // {
+            // text: 'Github',
+            // items: [
+            // {
+            // text: '前端源码',
+            // link: 'https://github.com/elunez/eladmin-web'
+            // },
+            // {
+            // text: '后端源码',
+            // link: 'https://github.com/elunez/eladmin'
+            // },
+            // ]
+            // },
+            // {
+            // text: 'Gitee',
+            // items: [
+            // {
+            // text: '前端源码',
+            // link: 'https://gitee.com/elunez/eladmin-web'
+            // },
+            // {
+            // text: '后端源码',
+            // link: 'https://gitee.com/elunez/eladmin'
+            // }
+            // ]
+            // }
+        ],
+        sidebar: [
             {
-                text: 'Github',
-                items: [
-                    {
-                        text: '前端源码',
-                        link: 'https://github.com/elunez/eladmin-web'
-                    },
-                    {
-                        text: '后端源码',
-                        link: 'https://github.com/elunez/eladmin'
-                    },
+                title: '心灵精神',   // 必要的
+                path: '/books/soul/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                    '/books/soul/1-思维的囚徒'
                 ]
             },
             {
-                text: 'Gitee',
-                items: [
-                    {
-                        text: '前端源码',
-                        link: 'https://gitee.com/elunez/eladmin-web'
-                    },
-                    {
-                        text: '后端源码',
-                        link: 'https://gitee.com/elunez/eladmin'
-                    }
+                title: '管理提升',
+                path: '/books/manager/',
+                collapsable: false, // 可选的,
+                sidebarDepth: 1,    // 可选的,
+                children: [
                 ]
-            }
+            },
+            {
+                title: '职场效能',
+                path: '/books/worker/',
+                collapsable: false, // 可选的,
+                sidebarDepth: 1,    // 可选的,
+                children: [
+                ]
+            },
+            {
+                title: '家庭培养',
+                path: '/books/family/',
+                collapsable: false, // 可选的,
+                sidebarDepth: 1,    // 可选的,
+                children: [
+                ]
+            },
+            {
+                title: '生活体验',
+                path: '/books/life/',
+                collapsable: false, // 可选的,
+                sidebarDepth: 1,    // 可选的,
+                children: [
+                ]
+            },
+            {
+                title: '历史人文',
+                path: '/books/history/',
+                collapsable: false, // 可选的,
+                sidebarDepth: 1,    // 可选的,
+                children: [
+                ]
+            },
+
         ],
-        sidebar: {
-            '/guide/': [
-                {
-                    title: '指南',
-                    collapsable: false,
-                    children: [
-                        '/guide/',
-                        '/guide/kslj',
-                        '/guide/ksks',
-                        '/guide/hdsc',
-                        '/guide/qdsc',
-                        '/guide/xmbs'
-                    ]
-                },
-                {
-                    title: '其它',
-                    collapsable: false,
-                    children: [
-                        '/guide/cjwt',
-                        '/guide/gxdm'
-                    ]
-                },
-                {
-                    title: '版权',
-                    collapsable: false,
-                    children: [
-                        '/guide/bqsm'
-                    ]
-                },
-                {
-                    title: '鸣谢',
-                    collapsable: false,
-                    children: [
-                        '/guide/mx'
-                    ]
-                }
-            ],
-            '/donation/': [
-                '/donation/',
-            ],
-            '/version/': [
-                {
-                    title: '更新日志',
-                    collapsable: false,
-                    children: [
-                        'V2.5',
-                        'V2.4'
-                    ]
-                }
-            ]
-        },
         // 假如你的文档仓库和项目本身不在一个仓库：
         docsRepo: 'elunez/eladmin-doc',
         // 假如文档不是放在仓库的根目录下：
--- docs/README.md
+++ docs/README.md
@@ -1,20 +1,20 @@
 ---
 home: true
-heroImage: /logo/logo.png
+heroImage: /logo/calvin_logo.png
 actionText: 快速开始 →
-actionLink: /guide/
+actionLink: /books/
 features:
-- title: 技术栈
-  details: 使用 SpringBoot、Jpa、Security、Redis、Vue 等前后端前沿技术开发。
-- title: 模块化
-  details: 后端采用按功能分模块开发方式，提升开发，测试效率。
-- title: 高效率
-  details: 项目简单可配，内置代码生成器，配置好表信息就能一键生成前后端代码。
-- title: 分离式
-  details: 前后端完全分离，前端基于 Vue，后端基于 Spring boot。
-- title: 响应式
-  details: 支持电脑、平板、手机等所有主流设备访问。
-- title: 易用性
-  details: 几乎可用于所有Web项目的开发，如 OA、Cms，网址后台管理等。
+- title: 心灵精神
+  details: 
+- title: 管理提升
+  details: 
+- title: 职场效能
+  details: 
+- title: 家庭培养
+  details: 
+- title: 生活体验
+  details: 
+- title: 历史人文
+  details: 
 footer:  Apache License 2.0 | Copyright © 2018-present Zheng Jie
 ---
